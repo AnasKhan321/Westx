2bbcc5d2a4b86bb97442179eb9282604711c11f3
Anas Khan <anascoder4@gmail.com>
2025-03-07 22:34:48 +0530

    changes

--- src/ReusableComponents/TokenDetail.tsx
+++ src/ReusableComponents/TokenDetail.tsx
@@ -1,7 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-
-import { Token } from "../utils/type";
 import SmallLoader from "./SmallLoader";
 import { useToken } from "../Context/TokenContext";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
@@ -20,12 +18,6 @@ interface TokenPrice {
     unit: string
 }
 
-
-const getTokenDetail = async (publicKey: string): Promise<Token> => {
-    const { data } = await axios.get<Token>(`https://believe-in-fun-backend-production.up.railway.app/api/tokens/mint/${publicKey}`)
-    return data;
-}
-
 const getMarketCap = async (publicKey: string) : Promise<MarketCap> => {
     const { data } = await axios.get<MarketCap>(`https://believe-in-fun-backend-production.up.railway.app/api/tokens/marketcap/${publicKey}`)
     console.log(data)
@@ -41,10 +33,6 @@ const getTokenPrice = async (publicKey: string) : Promise<TokenPrice> => {
 const TokenDetail = ({ publicKey }: { publicKey: string }) => {
 
     const { handlebuy, handleSell, isChecking } = useToken()
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ["tokenDetail", publicKey],
-        queryFn: () => getTokenDetail(publicKey)
-    })
 
     const {data : marketcapdata , isLoading : marketcaploading } = useQuery({
         queryKey: [`marketcap-${publicKey}`],
@@ -56,21 +44,20 @@ const TokenDetail = ({ publicKey }: { publicKey: string }) => {
         queryFn: () => getTokenPrice(publicKey)  , 
     })
 
-    if(isLoading){
-        return <div className="w-full h-full flex justify-center items-center">
+    if(marketcaploading || tokenpriceloading){
+        return 
             <SmallLoader />
-        </div>
+
     }
 
+
     return <div className="w-full mx-auto border border-white/40 py-8  mt-4 rounded-lg ">
-        {isError && <div className="w-full h-full flex justify-center items-center">
-            <p className="text-red-500">Error fetching token details</p>
-        </div>}
+
 
 
         <div className="flex gap-x-10 items-center flex-col">
             <div className="text-white/50 mb-6 text-lg italic  ">
-            {data?.market_cap == 0 ? <div> Be the First  one to buy  this token</div> : <div> You can even buy tokens of Persona now </div>}
+
        
             </div>
 
